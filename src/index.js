265ffc5d06dd83b004c9ab651bac598768b7fe6f
Nikita Gromov <nikita.gromoff@gmail.com>
2023-01-02 15:25:34 +0300

    update: pass tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = n - n * 2;
+        return +n.toString().split('').reverse().join('')
+    }
+    else return +n.toString().split('').reverse().join('')
 }
